b80a1b714d2ca69414e8bd702907a0363ba771b7
Nicolas Hovart <hovart.nicolas@gmail.com>
2024-02-11 23:33:19 +0100

    The way is the way

--- src/components/ResumeReminder/ResumeReminder.jsx
+++ src/components/ResumeReminder/ResumeReminder.jsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState, useSyncExternalStore } from "react";
 import "./ResumeReminder.css";
 import Draggable from 'react-draggable';
 import '../../assets/fonts/SF-Pro-Rounded-Regular.otf'
@@ -7,13 +7,26 @@ import data from '../../assets/data.json'
 import SidePanel from '../SidePanel/SidePanel.jsx'
 import MainPanel from '../MainPanel/MainPanel.jsx'
 
+const getScreenWidth = () => {
+    return 641
+}
+
 export const ResumeReminder = () => {
+    const [screenWidth, setScreenWidth] = useState(getScreenWidth())
+
     return (
+        // { screenWidth > 640 ?
         <Draggable>
             <section className="resume-reminder">
                 <MainPanel data={data} />
                 <SidePanel data={data} />
             </section>
         </Draggable>
+        //     :
+        // <section className="resume-reminder">
+        //     <MainPanel data={data} />
+        //     <SidePanel data={data} />
+        // </section>
+        // }
     )
 }
