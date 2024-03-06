21e3421f7784fe1d44fee4cb8e92980a5c18296f
Nicolas Hovart <hovart.nicolas@gmail.com>
2024-03-06 19:39:56 +0100

    the way is the way

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ni-co.dev',
   plugins: [
     react(),
-    vue({
-      // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
-      // For example <img src="/images/logo.png"> will not work without the code below
-      template: {
-        transformAssetUrls: {
-          includeAbsolute: false,
-          assetsInlineLimit: 0
-        },
-      },
-    })
   ]
 })
 
